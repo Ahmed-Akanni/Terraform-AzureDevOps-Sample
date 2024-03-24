60a6676a4441377de77c18bcb0ebba0bb0f9a22c
ahmed akanni <kayboy89@live.com>
2024-03-24 13:34:33 +0100

    yes

--- backend.tf
+++ backend.tf
@@ -5,7 +5,8 @@ terraform {
     container_name       = "prod-tfstate"
     key                  = "prod.terraform.tfstate"
   }
-}terraform {
+}
+terraform {
   backend "azurerm" {
     resource_group_name  = "demo-resources"
     storage_account_name = "kayak"
